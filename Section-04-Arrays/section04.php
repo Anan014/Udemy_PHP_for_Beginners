94acfbec15951a12f18020d3f257ad5bfe55d1ac
Anan Amer <ananamer95@gmail.com>
2024-11-17 12:16:15 +0200

    Section 04 Completed - Arrays

--- Section-04-Arrays/section04.php
+++ Section-04-Arrays/section04.php
@@ -1,1 +1,20 @@
-<!-- section 5 -->
+<!--
+Arrays
+
+Create a variable called array that contains an array with three simple values. These values can be whatever you like.
+
+Explicitly assign the string index of "third" to the third element.
+
+Write a foreach  loop that contains the following statement to print out each element of the array, along with its index:
+
+echo "$index = $value.";
+
+-->
+
+<?php
+
+$array = ["one" => "one", "second" => "two", "third" => "three"];
+
+foreach ($array as $index => $value) {
+    echo $index . " = " . $value . ".<br>";
+}
