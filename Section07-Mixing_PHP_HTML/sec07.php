3d21ab09b1dca2886afadb43a7734a4f68aceece
Anan Amer <ananamer95@gmail.com>
2024-11-21 20:49:56 +0200

    Section 07 Completed - Section 7: Mixing PHP and HTML

--- Section07-Mixing_PHP_HTML/sec07.php
+++ Section07-Mixing_PHP_HTML/sec07.php
@@ -1,1 +1,33 @@
-<!-- section 7 started -->
+<!-- 
+Mixing PHP and HTML
+The following script has some PHP at the top, followed by some HTML.
+An array variable is declared at the top that contains various elements.
+
+Add some PHP code inside the HTML to display the items from the array.
+They should be shown in an ordered list element (ol ). The resulting page in a browser should look like this:
+--> 
+
+<?php
+
+$fruit = ['apple', 'banana', 'orange', 'mango'];
+
+?>
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Fruit</title>
+    </head>
+    <body>
+
+    <h1>Fruit</h1>
+
+    <!-- put your code below this line -->
+    <ol>
+    <?php
+    foreach ($fruit as $value){
+      echo "<li>$value</li>";
+    }
+    ?>
+    </ol>
+    </body>
+</html>
