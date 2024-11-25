774ebdaa045bfa5f59d001cfb7ead05867edc36c
Anan Amer <ananamer95@gmail.com>
2024-11-25 22:52:27 +0200

    Section 08 Lec 45

--- Section08/sec08.php
+++ Section08/sec08.php
@@ -1,1 +1,15 @@
-<!-- section08 -->
+<?php
+
+$db_host = "localhost";
+$db_name = "cms";
+$db_user = "cms_www";
+$db_pass = "rhMD@]2T9.Zaca6J";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if(mysqli_connect_error()){
+    echo mysqli_connect_error();
+    exit;
+}
+
+echo "Connected successfully.";
