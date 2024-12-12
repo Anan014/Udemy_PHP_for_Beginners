7e8ce7e997a2bcf4acbcb40e7c9a31102916fa2f
Anan Amer <ananamer95@gmail.com>
2024-12-12 23:06:01 +0200

    section 10 completed - intro to html

---
+++ Section10-Intro_HTML_Forms/ex8.php
@@ -0,0 +1,41 @@
+<!-- 
+HTML Forms
+Add some HTML code below to create a form that looks like the image below:
+The form contains labels, a simple text input, a textarea and a select list.
+-->
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Form</title>
+</head>
+
+<body>
+
+    <h1>Form</h1>
+
+    <form>
+        <div>
+            <label>Name</label>
+            <input type="text" name="name" />
+        </div>
+        <div>
+            <label>Description</label>
+            <textarea></textarea>
+        </div>
+        <div>
+            <label>Size</label>
+            <select name="size">
+                <option value="One">One</option>
+                <option value="Two">Two</option>
+                <option value="Three">Three</option>
+            </select>
+        </div>
+
+
+    </form>
+
+</body>
+
+</html>
